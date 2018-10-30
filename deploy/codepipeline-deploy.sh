cd776a88276110178d661a098efaf6f10efaaf10
Fin Hopkins <fin.hopkins@boston.gov>
2018-10-30 15:09:46 -0400

    tmp

--- deploy/codepipeline-deploy.sh
+++ deploy/codepipeline-deploy.sh
@@ -2,10 +2,13 @@
 
 set -e
 
+echo Start
 # This should run on branches named "production-X" where "X" is the name of a 
 # service package of ours recognized by Lerna.
 [[ $CODEBUILD_INITIATOR =~ ^codepipeline\.(production|staging)\.([^/@]+) ]] || exit -1
 
 export SERVICE_NAME="${BASH_REMATCH[2]}"
 
-npx lerna run --stream --scope $SERVICE_NAME codepipeline-deploy
+echo "DEPLOY" $SERVICE_NAME
+
+# npx lerna run --stream --scope $SERVICE_NAME codepipeline-deploy
